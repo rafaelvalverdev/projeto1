4f2e964f603a239f5a3741504d46dd1784ddf30e
Valverde Filho <komoqcrafta@gmail.com>
2025-07-07 14:04:35 -0300

    Update app.py

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for  # type: ignore
 from config import Config
 from models import db, Tarefa
 
