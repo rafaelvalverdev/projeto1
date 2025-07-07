746f27bb317d5e791a33f224c0cc68bd1ffd42bf
Valverde Filho <komoqcrafta@gmail.com>
2025-07-07 13:38:39 -0300

    todolist 0.1

--- todo-app/config.py
+++ todo-app/config.py
@@ -5,10 +5,10 @@ class Config:
     # A SECRET_KEY é usada para segurança em sessões, CSRF, etc.
     # É importante para aplicações em produção.
     SECRET_KEY = os.environ.get(
-        'SECRET_KEY') or 'uma_chave_muito_secreta_e_complexa_que_voce_deve_mudar'
+        'SECRET_KEY') or '99334411'
 
     # String de conexão com o PostgreSQL
     # postgresql://usuario:senha@host:porta/nome_do_banco
-    SQLALCHEMY_DATABASE_URI = 'postgresql://todo_user:sua_senha_segura@localhost:5432/todo_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://todo_user:99334411@localhost:5432/todo_db'
     # Desativa o rastreamento de modificações do SQLAlchemy (consome menos memória)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
